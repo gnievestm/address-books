7b70b1c30fd2537b8f233f83e58029d5c301816a
Rafael <rafael.escobar@mercadolibre.com>
2023-09-15 12:53:02 -0300

    AddressBook Implementation

--- addressbookmanager/src/Main.java
+++ addressbookmanager/src/Main.java
@@ -1,13 +1,29 @@
+import Model.AddressBook;
 import Model.User;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
 
+        User user = new User(1, "pepe", "123455", "es pepe");
+        User user2 = new User(2, "Juan", "123455", "es juan");
+        User user3 = new User(3, "Ernesto", "123455", "es ernesto");
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addUser(user);
+        addressBook.addUser(user2);
+        addressBook.addUser(user3);
+
+        addressBook.deleteUser(user2);
+
+
 
     }
+
+
 }
