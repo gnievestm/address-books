24dc691c0204a01417ffc852fad9ee3e3b5a862b
leosilva12 <leonardo.silva@mercadolibre.com>
2023-09-15 12:51:26 -0300

    implement-addresbook
    incomplet

--- addressbookmanager/src/Main.java
+++ addressbookmanager/src/Main.java
@@ -1,13 +1,22 @@
+import Model.AddressBook;
 import Model.User;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        new AddressBook();
+        AddressBook addressBook = new AddressBook();
+        User pepe = new User(90, "leo","0","sasd");
 
+
+        addressBook.addUser(pepe);
+        Scanner teclado=new Scanner(System.in);
+
+        }
 
     }
 }
--- addressbookmanager/src/Model/AddressBook.java
+++ addressbookmanager/src/Model/AddressBook.java
@@ -1,22 +1,43 @@
 package Model;
+
 import java.util.List;
 
 public class AddressBook {
     private List<User> users;
 
     public AddressBook() {
-
+        this.users = null;
     }
 
     public void addUser(User user) {
-
+        for (int i=-1; i<users.size();i++){
+            if(users.get(i)!=user){
+                users.add(user);
+                }else{
+                System.out.println("ya existe este usuario");
+            }
+            }
     }
+
     public void deleteUser(User user) {
-
+        int pos=0;
+        for (int i=-1; i<users.size();i++){
+            if(users.get(i)==user){
+                pos=i;
+            }
+            users.remove(pos);
+        }
     }
+
     public User findUserByIdNumber(int idNumber) {
+       // for (int i=-1; i<users.size();i++){
+         //   if(users.get(i)==){
+
+           // }
+           // }
         throw new java.lang.UnsupportedOperationException("Not supported yet.");
     }
+
     public List<User> getUsers() {
         throw new java.lang.UnsupportedOperationException("Not supported yet.");
     }
