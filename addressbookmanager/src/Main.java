626dea9fd911402824a407e71fd50bb12c35eb6f
leosilva12 <leonardo.silva@mercadolibre.com>
2023-09-18 11:30:26 -0300

    complete my part of the code

--- addressbookmanager/src/Main.java
+++ addressbookmanager/src/Main.java
@@ -8,15 +8,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        new AddressBook();
+        //new AddressBook();
         AddressBook addressBook = new AddressBook();
-        User pepe = new User(90, "leo","0","sasd");
+        User user1 = new User(90, "leo","10","sasd");
+        User user2 = new User(91, "leonel","02","sasdd");
+        User user3 = new User(92, "leonard","23","sasddwe");
+        User user4 = new User(92, "leonard","23","sasddwe");
 
 
-        addressBook.addUser(pepe);
-        Scanner teclado=new Scanner(System.in);
 
+
+        addressBook.addUser(user1);
+        addressBook.addUser(user2);
+        addressBook.addUser(user3);
+        addressBook.addUser(user4);
+
+        addressBook.deleteUser(user4);
+
+        System.out.println(addressBook.findUserByIdNumber(92));
         }
 
     }
-}
--- addressbookmanager/src/Model/AddressBook.java
+++ addressbookmanager/src/Model/AddressBook.java
@@ -1,44 +1,46 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBook {
     private List<User> users;
 
     public AddressBook() {
-        this.users = null;
+        users = new ArrayList();
+
     }
 
     public void addUser(User user) {
-        for (int i=-1; i<users.size();i++){
-            if(users.get(i)!=user){
-                users.add(user);
-                }else{
-                System.out.println("ya existe este usuario");
-            }
-            }
+
+        User found=findUserByIdNumber(user.getIdNumber());
+        if(found==null){
+            users.add(user);
+        }
     }
 
     public void deleteUser(User user) {
-        int pos=0;
-        for (int i=-1; i<users.size();i++){
-            if(users.get(i)==user){
-                pos=i;
+        int pos = 0;
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i) == user) {
+                pos = i;
             }
             users.remove(pos);
         }
     }
 
     public User findUserByIdNumber(int idNumber) {
-       // for (int i=-1; i<users.size();i++){
-         //   if(users.get(i)==){
-
-           // }
-           // }
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
-    }
-
-    public List<User> getUsers() {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        for (int i = 0; i < users.size(); i++) {
+            if (idNumber == users.get(i).getIdNumber()) {
+                return users.get(i);
+            }
+        }
+        return null;
     }
 }
+
+
+//   public List<User> getUsers () {
+//  throw new java.lang.UnsupportedOperationException("Not supported yet.");
+//  }
+//}
